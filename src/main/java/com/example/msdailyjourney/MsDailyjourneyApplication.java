020638c27ba0211af66550fe0ce9b7583104ea82
Jacquin Grégoire <gregoire.jacquin@me.com>
2023-10-11 09:15:32 +0200

    Intial commit

---
+++ src/main/java/com/example/msdailyjourney/MsDailyjourneyApplication.java
@@ -0,0 +1,13 @@
+package com.example.msdailyjourney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsDailyjourneyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsDailyjourneyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/msdailyjourney/MsDailyjourneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.msdailyjourney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsDailyjourneyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
